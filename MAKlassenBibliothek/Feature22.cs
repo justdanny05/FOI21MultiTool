c6390bedfa7a4ce7cc4b1cc5e0625bc3c8835095
Sumiyabazar Gerelt Od <sumiyabaza.g@gso.schule.koeln>
2023-03-23 12:43:24 +0100

    update

---
+++ MAKlassenBibliothek/Feature22.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MAKlassenBibliothek
+{
+    internal class Feature22
+    {
+        internal static void Feature_22 ()
+        {
+            Console.WriteLine("Test Feature 1");
+        }
+    }
+}
