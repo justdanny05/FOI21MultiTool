b0cd12affebb7d1abc8440b661d595745daf92af
Selim Afat <selimafat@outlook.de>
2023-03-24 22:56:33 +0100

    Neue und coole Elemente eingefügt V 1.1

--- WIKlassenBibliothek/Feature16.cs
+++ WIKlassenBibliothek/Feature16.cs
@@ -9,71 +9,97 @@ namespace WIKlassenBibliothek
     {
         internal static void Feature_16()
         {
-            Console.BackgroundColor = ConsoleColor.Blue;
-            Console.CursorSize = 100;
-            Console.ForegroundColor = ConsoleColor.White;
+
             bool Exit = false;
 
             string username;
             Console.WriteLine("Geben Sie Ihren Namen ein:");
             username = Console.ReadLine();
 
+
+            Console.WriteLine("Geben Sie 'start' ein, um den Währungsrechner zu starten. Geben Sie 'exit' ein, um das tool zu schliessen.");
             do
             {
-                Console.Clear();
-                Console.WriteLine();
-                Console.WriteLine(FiggleFonts.Slant.Render("Währungsrechner"));
-                Console.Title = "Währungsrechner";
-                Console.WriteLine("------------------------------------------------------------------------------------\n");
-                Console.WriteLine("Eingabe: exit\t        ->\tbeendet das Programm");
-                Console.WriteLine("Eingabe: toolexit\t->\tbeendet das Tool");
-                Console.WriteLine("Eingabe: help\t->\tSie brauchen Hilfe?");
-                Console.WriteLine("\n\nHi " + username + ", ich wurde von Selim programmiert und ich rechne für Sie Währungen um.\n\n");
+                string input = Console.ReadLine();
 
-                string Währung1;
-                string Währung2;
-                double betrag;
-
-                Console.WriteLine("Die erste Währung, die Sie eingeben, wird in die zweite umgerechnet.\n");
-                Console.Write("Geben Sie die erste Währung (z.B. USD): ");
-                Währung1 = Console.ReadLine().ToUpper();
-                Console.Write("Geben Sie die zweite Währung (z.B. EUR): ");
-                Währung2 = Console.ReadLine().ToUpper();
-                Console.Write("Geben Sie den Betrag ein, der umgerechnet werden soll: ");
-                betrag = Convert.ToDouble(Console.ReadLine());
-
-                // API request to Fixer.io
-
-                // API request to Fixer.io
-                var client = new RestClient($"https://api.apilayer.com/fixer/convert?from={Währung1}&to={Währung2}&amount={betrag}");
-                client.Timeout = -1;
-                var request = new RestRequest(Method.GET);
-                request.AddHeader("apikey", "11jkM650waV1JUSKCDDDzUXqGHHoHHRI");
-                IRestResponse response = client.Execute(request);
-
-                if (response.IsSuccessful)
+                if (input.ToLower() == "start")
                 {
-                    var data = JObject.Parse(response.Content);
+                    do
+                    {
+                        Console.Clear();
+                        Console.WriteLine();
+                        Console.WriteLine(FiggleFonts.Slant.Render("Währungsrechner"));
+                        Console.Title = "Währungsrechner";
+                        Console.WriteLine("------------------------------------------------------------------------------------\n");
+                        Console.WriteLine("Taste: e\t->\tbeendet das Programm");
+                        Console.WriteLine("\n\nHi " + username + ", ich wurde von Selim programmiert und ich rechne für Sie Währungen um.\n\n");
 
-                    if (data["success"].ToObject<bool>())
-                    {
-                        double result = (double)data["result"];
-                        Console.WriteLine($"\n{betrag} {Währung1} entspricht {result} {Währung2}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("\nFehler beim Abrufen der Wechselkurse.");
-                    }
+                        string Währung1;
+                        string Währung2;
+                        double betrag;
+
+
+                        Console.WriteLine("Die erste Währung, die Sie eingeben, wird in die zweite umgerechnet.\n");
+                        Console.Write("Geben Sie die erste Währung (z.B. USD): ");
+                        Währung1 = Console.ReadLine().ToUpper();
+                        Console.Write("Geben Sie die zweite Währung (z.B. EUR): ");
+                        Währung2 = Console.ReadLine().ToUpper();
+                        Console.Write("Geben Sie den Betrag ein, der umgerechnet werden soll: ");
+                        betrag = Convert.ToDouble(Console.ReadLine());
+
+
+                        var client = new RestClient($"https://api.apilayer.com/fixer/convert?from={Währung1}&to={Währung2}&amount={betrag}");
+                        client.Timeout = -1;
+                        var request = new RestRequest(Method.GET);
+                        request.AddHeader("apikey", "11jkM650waV1JUSKCDDDzUXqGHHoHHRI");
+                        IRestResponse response = client.Execute(request);
+
+                        if (response.IsSuccessful)
+                        {
+                            var data = JObject.Parse(response.Content);
+
+                            if (data["success"].ToObject<bool>())
+                            {
+                                double result = Math.Round((double)data["result"], 2);
+                                Console.WriteLine($"\n{betrag} {Währung1} entspricht {result} {Währung2}");
+                            }
+                            else
+                            {
+                                Console.WriteLine("\nFehler beim Abrufen der Wechselkurse.");
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine($"\nFehler beim Abrufen der Wechselkurse. Status code: {response.StatusCode}");
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine();
+                        Console.WriteLine("Danke " + username + " Das du mein tool genutzt hast!");
+                        Console.WriteLine();
+                        Console.WriteLine("\nWenn du das Programm beenden möchtest, drücke 'e'. Wenn du Fortsetzen möchtest drücke eine beliebige andere taste.");
+                        string userInput = Console.ReadLine().ToLower();
+                        if (userInput == "e")
+                        {
+                            Exit = true;
+                            Console.Clear();
+                        }
+
+
+                    } while (!Exit);
+                }
+                else if (input.ToLower() == "exit")
+                {
+                    Exit = true;
+                    Console.Clear();
                 }
                 else
                 {
-                    Console.WriteLine($"\nFehler beim Abrufen der Wechselkurse. Status code: {response.StatusCode}");
+                    Console.WriteLine("Ungültige Eingabe. Geben Sie 'start' ein, um den Währungsrechner zu starten. Geben Sie 'exit' ein, um das tool zu schliessen");
                 }
-
-
-                Console.WriteLine("\nDrücken Sie eine beliebige Taste, um fortzufahren...");
-                Console.ReadKey();
             } while (!Exit);
+
+
+
         }
     }
 }
