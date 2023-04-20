c7ebb9e51b4d1713da41684a53d614eef93bc441
Paul Schmitt <paul.s98@gso.schule.koeln>
2023-04-20 12:26:48 +0200

    a

---
+++ ETKlassenBibliothek/Feature1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Feature1
+{
+	public Feature1()
+	{
+        internal static void Feature_1()
+        {
+            Console.WriteLine("Test Feature 1");
+            //Diese Ausgabe hilft Ihnen zu erkennen ob der Aufruf funktioniert.
+        }
+    }
+}
