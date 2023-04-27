194ee17618481860e8ee17beb20d029bbd039b42
Simon Blümel <simon.b62@gso.schule.koeln>
2023-04-27 12:49:08 +0200

    Feature9 anfang: Überschrift

---
+++ PHKlassenBibliothek/Feature9.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHKlassenBibliothek
+{
+    internal class Feature9
+    {
+        internal static void Feature_9()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(@"
+_________                 ______           _____       _____________        ______      __________ 
+__  ____/____________________  /____      ____(_)____________  /__(_)______ ___  /_________(_)_  /_
+_  / __ _  _ \_  ___/  ___/_  __ \_ | /| / /_  /__  __ \  __  /__  /__  __ `/_  //_/  _ \_  /_  __/
+/ /_/ / /  __/(__  )/ /__ _  / / /_ |/ |/ /_  / _  / / / /_/ / _  / _  /_/ /_  ,<  /  __/  / / /_  
+\____/  \___//____/ \___/ /_/ /_/____/|__/ /_/  /_/ /_/\__,_/  /_/  _\__, / /_/|_| \___//_/  \__/  
+                                                                    /____/                         ");
+            Console.ReadKey();
+        }
+       
+    }
+}
