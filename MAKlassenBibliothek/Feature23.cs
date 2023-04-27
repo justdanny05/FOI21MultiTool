d51ae9fded21d5431dddd476566c9131d32041d1
Yonatan Tedros <yonatan.t@gso.schule.koeln>
2023-04-27 12:17:08 +0200

    programm überarbeitet

--- MAKlassenBibliothek/Feature23.cs
+++ MAKlassenBibliothek/Feature23.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,25 +11,34 @@ namespace MAKlassenBibliothek
     {
         internal static void Feature_23()
         {
-            Console.Clear();
+            string wiederholen = "n";
+            do
+            {
+                double distance, speed, timeInHours, timeInMinutes, timeInSeconds;
 
-            double distance, speed, timeInHours, timeInMinutes, timeInSeconds;
+                Console.WriteLine("Geben sie ihre Distanze in Kilometer an:");
+                distance = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Geben sie ihre Distanze in Kilometer an:");
-            distance = double.Parse(Console.ReadLine());
+                Console.WriteLine("Geben sie ihre Geschwindigkeit in km/h an:");
+                speed = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Geben sie ihre Geschwindigkeit in km/h an:");
-            speed = double.Parse(Console.ReadLine());
+                timeInHours = distance / speed;
+                timeInMinutes = timeInHours * 60;
+                timeInSeconds = timeInMinutes * 60;
 
-            timeInHours = distance / speed;
-            timeInMinutes = timeInHours * 60;
-            timeInSeconds = timeInMinutes * 60;
+                Console.WriteLine("{0} Stunden", timeInHours);
+                Console.WriteLine("{0} minuten", timeInMinutes);
+                Console.WriteLine("{0} sekunden", timeInSeconds);
 
-            Console.WriteLine("{0} Stunden", timeInHours);
-            Console.WriteLine("{0} minuten", timeInMinutes);
-            Console.WriteLine("{0} sekunden", timeInSeconds);
+                Console.ReadLine();
 
-            Console.ReadLine();
+                Console.WriteLine("möchten sie das programm wiederholen ?");
+                wiederholen=Console.ReadLine();
+                Console.Clear();
+
+            } while(wiederholen == "j");  
+
+            
 
         }
     }
