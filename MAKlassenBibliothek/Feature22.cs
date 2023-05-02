f95304928f92f0f46346306b92263ed945ab0297
Sumiyabazar Gerelt Od <sumiyabaza.g@gso.schule.koeln>
2023-05-02 21:03:52 +0200

    fertig!!!!

--- MAKlassenBibliothek/Feature22.cs
+++ MAKlassenBibliothek/Feature22.cs
@@ -15,110 +15,159 @@ namespace MAKlassenBibliothek
             bool weiterSpielen = true;
             int punkte = 0;
             int gesamtFragen = 0;
-            int punkteWert = 1;
             int schwierigkeitsgrad = 1;
+
 
             while (weiterSpielen)
             {
-
-                Console.WriteLine("Math QueZZee ");
-                Console.Write($"Geben Sie den Schwierigkeitsgrad ein (1-5, {schwierigkeitsgrad} ist aktuell ausgewählt): ");
-                int eingabeSchwierigkeitsgrad = int.Parse(Console.ReadLine());
-
-                if (eingabeSchwierigkeitsgrad < 1 || eingabeSchwierigkeitsgrad > 5)
+                Console.Clear();
+                Console.WriteLine("=== Hauptmenü ===");
+                Console.WriteLine("1. Spiel starten");
+                Console.WriteLine("2. Beenden");
+                Console.WriteLine($"{"Dein punkte :"}{punkte}");
+                Console.WriteLine("***************Info******************");
+                Console.WriteLine("Wenn Sie Während des Spiels ;exit; eingeben, können Sie das Programm verlassen ");
+                Console.Write("Bitte wählen Sie eine Option: ");
+                string eingabe = Console.ReadLine();
+                switch (eingabe)
                 {
-                    Console.WriteLine("Ungültiger Schwierigkeitsgrad. Bitte geben Sie eine Zahl zwischen 1 und 5 ein.");
-                    continue;
+                    case "1":
+
+                        Console.Write($"Geben Sie den Schwierigkeitsgrad ein (1-5, {schwierigkeitsgrad} ist aktuell ausgewählt): ");
+                        int eingabeSchwierigkeitsgrad = 0;
+                        string Input = Console.ReadLine();
+                        try
+                        {
+                            eingabeSchwierigkeitsgrad = int.Parse(Input);
+
+                        }
+                        catch (FormatException ex)
+                        {
+                            if (Input == "exit")
+                            {
+
+                                Console.WriteLine("Auf Wiedersehen!");
+                                return;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Fk you !!!!");
+                                continue;
+                            }
+
+                        }
+
+                        if (eingabeSchwierigkeitsgrad < 1 || eingabeSchwierigkeitsgrad > 5)
+                        {
+                            Console.WriteLine("Ungültiger Schwierigkeitsgrad. Bitte geben Sie eine Zahl zwischen 1 und 5 ein.");
+                            continue;
+                        }
+
+                        schwierigkeitsgrad = eingabeSchwierigkeitsgrad;
+
+                        Random zufall = new Random();
+
+                        int anzahlOperanden = zufall.Next(schwierigkeitsgrad + 1, schwierigkeitsgrad + 4);
+                        int[] operanden = new int[anzahlOperanden];
+
+                        for (int i = 0; i < anzahlOperanden; i++)
+                        {
+                            operanden[i] = zufall.Next(1, schwierigkeitsgrad * 5 + 1);
+                        }
+
+                        int anzahlOperatoren = anzahlOperanden - 1;
+                        char[] operatoren = new char[anzahlOperatoren];
+
+                        for (int i = 0; i < anzahlOperatoren; i++)
+                        {
+                            int operatorIndex = zufall.Next(0, 4);
+
+                            switch (operatorIndex)
+                            {
+                                case 0:
+                                    operatoren[i] = '+';
+                                    break;
+                                case 1:
+                                    operatoren[i] = '-';
+                                    break;
+                                case 2:
+                                    operatoren[i] = '*';
+                                    break;
+                                case 3:
+                                    operatoren[i] = '/';
+                                    break;
+                            }
+                        }
+
+                        string ausdruck = "";
+
+                        for (int i = 0; i < anzahlOperanden; i++)
+                        {
+                            ausdruck += operanden[i].ToString();
+
+                            if (i < anzahlOperatoren)
+                            {
+                                ausdruck += operatoren[i].ToString();
+                            }
+                        }
+
+                        Console.Write($"Was ist {ausdruck}? ");
+                        string eingabeAntwort = Console.ReadLine();
+
+                        if (eingabeAntwort == "exit")
+                        {
+
+                            Console.WriteLine("Auf Wiedersehen!");
+                            return;
+                        }
+
+                        int antwort = int.Parse(eingabeAntwort);
+
+                        int ergebnis = operanden[0];
+
+                        for (int i = 0; i < anzahlOperatoren; i++)
+                        {
+                            switch (operatoren[i])
+                            {
+                                case '+':
+                                    ergebnis += operanden[i + 1];
+                                    break;
+                                case '-':
+                                    ergebnis -= operanden[i + 1];
+                                    break;
+                                case '*':
+                                    ergebnis *= operanden[i + 1];
+                                    break;
+                                case '/':
+                                    ergebnis /= operanden[i + 1];
+                                    break;
+                            }
+                        }
+
+                        if (antwort == ergebnis)
+                        {
+                            Console.WriteLine("Richtig!");
+                            punkte += 1;
+                            gesamtFragen++;
+                            punkte = +2;
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Falsch. Die richtige Antwort ist :{ergebnis}");
+
+
+
+
+                        }
+
+                        break;
+                    case "2":
+                        Console.WriteLine("Programm wird beendet...");
+                        weiterSpielen = false;
+                        break;
+
                 }
-
-                schwierigkeitsgrad = eingabeSchwierigkeitsgrad;
-
-                Random zufall = new Random();
-
-                int anzahlOperanden = zufall.Next(schwierigkeitsgrad + 1, schwierigkeitsgrad + 4);
-                int[] operanden = new int[anzahlOperanden];
-
-                for (int i = 0; i < anzahlOperanden; i++)
-                {
-                    operanden[i] = zufall.Next(1, schwierigkeitsgrad * 5 + 1);
-                }
-
-                int anzahlOperatoren = anzahlOperanden - 1;
-                char[] operatoren = new char[anzahlOperatoren];
-
-                for (int i = 0; i < anzahlOperatoren; i++)
-                {
-                    int operatorIndex = zufall.Next(0, 4);
-
-                    switch (operatorIndex)
-                    {
-                        case 0:
-                            operatoren[i] = '+';
-                            break;
-                        case 1:
-                            operatoren[i] = '-';
-                            break;
-                        case 2:
-                            operatoren[i] = '*';
-                            break;
-                        case 3:
-                            operatoren[i] = '/';
-                            break;
-                    }
-                }
-
-                string ausdruck = "";
-
-                for (int i = 0; i < anzahlOperanden; i++)
-                {
-                    ausdruck += operanden[i].ToString();
-
-                    if (i < anzahlOperatoren)
-                    {
-                        ausdruck += operatoren[i].ToString();
-                    }
-                }
-
-                Console.Write($"Was ist {ausdruck}? ");
-                int antwort = int.Parse(Console.ReadLine());
-
-                int ergebnis = operanden[0];
-
-                for (int i = 0; i < anzahlOperatoren; i++)
-                {
-                    switch (operatoren[i])
-                    {
-                        case '+':
-                            ergebnis += operanden[i + 1];
-                            break;
-                        case '-':
-                            ergebnis -= operanden[i + 1];
-                            break;
-                        case '*':
-                            ergebnis *= operanden[i + 1];
-                            break;
-                        case '/':
-                            ergebnis /= operanden[i + 1];
-                            break;
-                    }
-                }
-
-                if (antwort == ergebnis)
-                {
-                    Console.WriteLine("Richtig!");
-                    punkte += punkteWert;
-                }
-                else
-                {
-                    Console.WriteLine($"Falsch!. Die richtige Antwort ist {ergebnis}.");
-                }
-
-                gesamtFragen++;
-                punkteWert += 2;
-
-                Console.WriteLine($"Aktueller Punktestand: {punkte}");
-                Console.WriteLine($"Gesamte Fragen: {gesamtFragen}");
-
+                Console.ReadKey();
 
             }
         }
