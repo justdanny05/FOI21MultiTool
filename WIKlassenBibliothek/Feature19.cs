f818a7d0bac2edb0a8cf10c3d3a565c40efb020c
Maryam Amiri <maryam.a28@gso.schule.koeln>
2023-05-03 16:36:17 +0200

    erforderlich

--- WIKlassenBibliothek/Feature19.cs
+++ WIKlassenBibliothek/Feature19.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,8 +15,11 @@ namespace WIKlassenBibliothek
             while (repeatProgrm)
             {
 
-                Console.WriteLine($"{"---Amortisationsrechner---"}");
-                Console.WriteLine("");
+                Console.WriteLine("------------------------------------------------------------------------------------\n" +
+                                  "                              >>> Amortisationsrechner <<<\n" +
+                                  "------------------------------------------------------------------------------------\n\n");
+
+                Console.WriteLine($"{"Mit diesem Programm können sie berechnen, wie viel sie im Monat verkaufen müssen damit sie keine verlust machen.\n"}");
 
                 string fixkosten;
                 int fixkosten_int;
@@ -36,7 +40,7 @@ namespace WIKlassenBibliothek
                     isNumeric = int.TryParse(fixkosten, out fixkosten_int);
                     if (!isNumeric)
                     {
-                        Console.WriteLine("Die Eingabe war ungültig. Bitte geben Sie Ihre Fixkosten nochmal ein:");
+                        Console.WriteLine("Die Eingabe war ungültig.Bitte geben Sie Ihre Fixkosten nochmal ein:\n");
                         Console.ReadKey();
                        
                     }
@@ -44,13 +48,14 @@ namespace WIKlassenBibliothek
 
                 do
                 {
-                    Console.WriteLine("Bitte geben Sie Ihre Variable Kosten ein: ");
+                    Console.WriteLine("Bitte geben Sie Ihre Variable Kosten ein:\n");
                     variablekosten = Console.ReadLine();
                     isNumeric = int.TryParse(variablekosten, out variablekosten_int);
 
                     if (!isNumeric)
                     {
-                        Console.WriteLine("Die Eingabe war ungültig. \nBitte geben Sie Ihre Variable kosten noch mal ein:");
+                        Console.WriteLine("Die Eingabe war ungültig.Bitte geben Sie Ihre Variable kosten noch mal ein:\n");
+                        Console.ReadKey();
                     }
 
                 } while (!isNumeric);
@@ -58,13 +63,14 @@ namespace WIKlassenBibliothek
                 do
                 {
                     
-                    Console.WriteLine("Bitte geben Sie Ihre Preis ein: ");
+                    Console.WriteLine("Bitte geben Sie Ihre Preis ein:\n");
                     preis = Console.ReadLine();
                     isNumeric = int.TryParse(preis, out preis_int);
 
                     if (!isNumeric)
                     {
-                        Console.WriteLine("Die Eingabe war ungültig. \nBitte geben Sie Ihre Preis nochmal ein:");
+                        Console.WriteLine("Die Eingabe war ungültig.Bitte geben Sie Ihre Preis nochmal ein:\n");
+                        Console.ReadKey();
                     }
                 } while (!isNumeric);
 
@@ -72,8 +78,23 @@ namespace WIKlassenBibliothek
 
                 double menge = fixkosten_int + (variablekosten_int / preis_int);
 
-                Console.WriteLine();
-                Console.WriteLine($"Die Menge beträgt: {menge}");
+                Console.WriteLine($"Die Menge die verkauft werden muss beträgt: {menge}\n");
+
+                Console.WriteLine("Wollen Sie das Programm wiederholen? (Ja = 1 / Nein = 2)\n");
+                int antwort_int = Convert.ToInt16(Console.ReadLine());
+
+                if (antwort_int == 1) 
+                    {
+                    Console.WriteLine("Das Programm wird Wiederholt.\n");
+                    repeatProgrm = true;
+                    
+                    }  
+                else if (antwort_int == 2)
+                    {
+
+                    repeatProgrm = false;
+                    
+                    }
 
 
 
