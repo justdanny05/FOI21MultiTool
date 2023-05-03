2b36ded2cc4a7cda0056e922c11cb529294c301c
Mazen Fetouh Abdel Hadi Abdel Wahed Awadalla <mazenfetou.a@gso.schule.koeln>
2023-05-03 22:31:28 +0200

    4. Auswahl hinzugefügt, Code fertiggestellt

--- MAKlassenBibliothek/Feature24.cs
+++ MAKlassenBibliothek/Feature24.cs
@@ -1,31 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MAKlassenBibliothek
+﻿namespace MAKlassenBibliothek
 {
     internal class Feature24
     {
         internal static void Feature_24()
         {
-            Console.BackgroundColor = ConsoleColor.Black;
-            Console.ForegroundColor = ConsoleColor.Green;
-
             while (true)
             {
-                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Koordinatenbasierter Rechner\n");
                 Console.WriteLine("Bitte wählen Sie eine Option aus:");
-                Console.WriteLine("1) Abstand zwischen zwei Punkten berechnen");
+                Console.WriteLine("1) Abstand zwichen zwei Punkten berechnen");
                 Console.WriteLine("2) Mittelpunkt einer Strecke berechnen");
-                Console.WriteLine("3) Beenden");
+                Console.WriteLine("3) Steigung zwischen zwei Punkten berechnen");
 
-                string input = Console.ReadLine();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("4) Beenden\n");
+                Console.ForegroundColor = ConsoleColor.Blue;
+                string Eingabe = Console.ReadLine();
 
-                if (input == "1")
+                if (Eingabe == "1")
                 {
-                    Console.WriteLine("~~~~~--Abstand zwischen zwei Punkten auf einer Ebene berechnen.--~~~~~\n");
+                    Console.Clear();
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("~~~~~--Abstand zwischen zwei Punkten auf einer Ebene berechnen--~~~~~\n");
 
                     Console.WriteLine("Gib die Koordinaten von Punkt 1 ein (x1, y1):");
                     double x1 = Convert.ToDouble(Console.ReadLine());
@@ -36,16 +34,19 @@ namespace MAKlassenBibliothek
                     double y2 = Convert.ToDouble(Console.ReadLine());
 
                     double distance = CalculateDistance(x1, y1, x2, y2);
-
                     Console.WriteLine("Der Abstand zwischen den beiden Punkten beträgt {0}", distance);
-
                     Console.WriteLine("Drücken Sie eine beliebige Taste, um fortzufahren...");
                     Console.ReadKey();
-                }
-                else if (input == "2")
-                {
-                    Console.WriteLine("~~~~~--Berechnung des Mittelpunkts einer Strecke--~~~~~\n");
+                    Console.Clear();
 
+
+                }
+                else if (Eingabe == "2")
+                {
+                    Console.Clear();
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("~~~~~--Berechnung des Mittelpunkts einer Strecke--~~~~~\n");
                     Console.WriteLine("Gib die Koordinaten von Punkt 1 ein (x1, y1):");
                     double x1 = Convert.ToDouble(Console.ReadLine());
                     double y1 = Convert.ToDouble(Console.ReadLine());
@@ -53,39 +54,62 @@ namespace MAKlassenBibliothek
                     Console.WriteLine("Gib die Koordinaten von Punkt 2 ein (x2, y2):");
                     double x2 = Convert.ToDouble(Console.ReadLine());
                     double y2 = Convert.ToDouble(Console.ReadLine());
-
                     double midX = (x1 + x2) / 2;
                     double midY = (y1 + y2) / 2;
 
                     Console.WriteLine("Der Mittelpunkt der Strecke liegt bei ({0}, {1})", midX, midY);
+                    Console.WriteLine("Drücken Sie eine beliebige Taste, um fortzufahren...");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+                else if (Eingabe == "3")
+                {
+                    Console.Clear();
+                    Console.WriteLine("");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("~~~~~--Berechnung der Steigung zwischen zwei Punkten auf einer Ebene--~~~~~\n");
+                    Console.WriteLine("Gib die Koordinaten von Punkt 1 ein (x1, y1):");
+                    double x1 = Convert.ToDouble(Console.ReadLine());
+                    double y1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Gib die Koordinaten von Punkt 2 ein (x2, y2):");
+                    double x2 = Convert.ToDouble(Console.ReadLine());
+                    double y2 = Convert.ToDouble(Console.ReadLine());
+
+                    if (x2 - x1 == 0)
+                    {
+                        Console.WriteLine("Die Steigung ist unendlich");
+                    }
+                    else
+                    {
+                        double steigung = (y2 - y1) / (x2 - x1);
+                        Console.WriteLine("Die Steigung zwischen den beiden Punkten beträgt {0}", steigung);
+                    }
 
                     Console.WriteLine("Drücken Sie eine beliebige Taste, um fortzufahren...");
                     Console.ReadKey();
+                    Console.Clear();
                 }
-                else if (input == "3")
+                else if (Eingabe == "4")
                 {
-                    Console.WriteLine("Das Programm wird beendet...");
+                    Console.WriteLine("Programm beendet");
                     break;
+
                 }
                 else
                 {
-                    Console.WriteLine("Ungültige Eingabe! Bitte wählen Sie eine gültige Option aus.");
+                    Console.WriteLine("Ungültige Eingabe. Bitte wählen Sie eine Option aus dem Menü aus.");
                     Console.WriteLine("Drücken Sie eine beliebige Taste, um fortzufahren...");
                     Console.ReadKey();
                 }
             }
 
-            Console.ResetColor();
-        }
+            static double CalculateDistance(double x1, double y1, double x2, double y2)
+            {
+                double distance = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+                return distance;
 
-        static double CalculateDistance(double x1, double y1, double x2, double y2)
-        {
-            double deltaX = x2 - x1;
-            double deltaY = y2 - y1;
 
-            double distance = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
-
-            return distance;
+            }
         }
     }
 }
